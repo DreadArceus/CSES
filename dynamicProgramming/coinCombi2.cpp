4add4af2c136da47f83b7f5436ff657fcccd273e
dreadarceus <dreadarceus@gmail.com>
2021-04-08 02:14:06 +0530

    feat: coin 2

---
+++ dynamicProgramming/coinCombi2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int solve(vector<int> nums, int target)
+{
+    int dp[target + 1];
+    memset(dp, 0, sizeof(dp));
+    dp[0] = 1;
+    for (int j = 0; j < nums.size(); j++)
+    {
+        for (int i = 1; i <= target; i++)
+        {
+            if (i >= nums[j])
+            {
+                dp[i] += dp[i - nums[j]] % int(1e9 + 7);
+                dp[i] %= int(1e9 + 7);
+            }
+        }
+    }
+    return dp[target];
+}
+
+int main()
+{
+    int n = 0, target = 0;
+    cin >> n >> target;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    int ans = solve(nums, target);
+    cout << ans;
+}
